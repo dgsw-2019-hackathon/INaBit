b461f1416bb01af9f9e5f265cd1ce7d41f64d9ef
ReturnYoung <48465460+RturnYoung@users.noreply.github.com>
2019-07-16 14:48:36 +0900

    gsstry

--- INaBit/Controls/JoinControl.xaml.cs
+++ INaBit/Controls/JoinControl.xaml.cs
@@ -18,9 +18,9 @@ namespace INaBit.Controls
     /// <summary>
     /// UserControl2.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class UserControl2 : UserControl
+    public partial class JoinControl : UserControl
     {
-        public UserControl2()
+        public JoinControl()
         {
             InitializeComponent();
         }
--- INaBit/Controls/WebPostControl.xaml.cs
+++ INaBit/Controls/WebPostControl.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace INaBit.Controls
 {
     /// <summary>
-    /// UserControl1.xaml에 대한 상호 작용 논리
+    /// UserControl3.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class LoginControl : UserControl
+    public partial class WebPostControl : UserControl
     {
-        public LoginControl()
+        public WebPostControl()
         {
             InitializeComponent();
         }
