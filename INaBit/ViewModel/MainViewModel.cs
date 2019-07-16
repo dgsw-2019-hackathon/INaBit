27790e2829416cce8dc7dbf37d959ce0dd5fbe58
hbmin3789 <38002183+mokhano@users.noreply.github.com>
2019-07-16 17:51:30 +0900

    cuv

---
+++ INaBit/ViewModel/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace INaBit.ViewModel
+{
+    public class MainViewModel : BindableBase
+    {
+        public ICommand IdeaPageCommand { get; set; }
+        public ICommand WebPageCommand { get; set; }
+        public ICommand AppPageCommand { get; set; }
+
+        public MainViewModel()
+        {
+            IdeaPageCommand = new DelegateCommand(IdeaPage);
+            WebPageCommand = new DelegateCommand(WebPage);
+            AppPageCommand = new DelegateCommand(AppPage);
+        }
+
+        private void AppPage()
+        {
+            
+        }
+
+        private void WebPage()
+        {
+            
+        }
+
+        private void IdeaPage()
+        {
+            
+        }
+    }
+}
