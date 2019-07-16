45456fbdcf7354532b5657adbcde88571c8e84d2
hbmin3789 <38002183+mokhano@users.noreply.github.com>
2019-07-16 13:35:09 +0900

    asd

---
+++ INaBit/ViewModel/Posts/PostItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INaBit.ViewModel.Posts
+{
+    class PostItemViewModel : BindableBase
+    {
+        private string _category;
+        public string Category
+        {
+            get => _category;
+            set => SetProperty(ref _category, value);
+        }
+
+    }
+}
---
+++ INaBit/ViewModel/Posts/PostsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using INaBit.Controls.Posts;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INaBit.ViewModel.Posts
+{
+    class PostsViewModel : BindableBase
+    {
+        private ObservableCollection<PostItemControl> _items;
+        public ObservableCollection<PostItemControl> Items
+        {
+            get => _items;
+            set => SetProperty(ref _items, value);
+        }
+
+        public PostsViewModel()
+        {
+            Items = new ObservableCollection<PostItemControl>();
+            Items.Add(new PostItemControl());
+        }
+
+    }
+}
