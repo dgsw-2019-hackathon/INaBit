658a2b4505f468b21eaa84bfbcd3ae9ce4b7cb7b
ReturnYoung <48465460+RturnYoung@users.noreply.github.com>
2019-07-16 17:15:02 +0900

    dd

---
+++ INaBit/Controls/AppPostWriteControl.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace INaBit.Controls
+{
+    /// <summary>
+    /// AppPostWriteControl.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class AppPostWriteControl : UserControl
+    {
+        public string filename = null;
+        public AppPostWriteControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.DefaultExt = ".png";
+            dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                this.filename = dlg.FileName;
+                text1.Text = filename;
+            }
+        }
+    }
+}
